5c4b2a673839cf17d855b3a281d64d0189ce1a67
robertalanis <rxalanis@gmail.com>
2020-09-01 00:59:31 -0500

    Updates

---
+++ precache-manifest.fe15629853b412a66edce7bd479f25d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c7ea03f37f20598ff232d516aaf17d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5bec571ab91399f051d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8504a2e359921dee40cd",
+    "url": "/static/css/main.6973b9c7.chunk.css"
+  },
+  {
+    "revision": "d5bec571ab91399f051d",
+    "url": "/static/js/2.474631ea.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.474631ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8504a2e359921dee40cd",
+    "url": "/static/js/main.e9f0ba2c.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);
