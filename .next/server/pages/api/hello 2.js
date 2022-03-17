373b425de98943570f27f0093c6611412caa988d
Robert Xavier Alanis <59277298+robertalanis@users.noreply.github.com>
2022-03-17 16:41:27 -0500

    preparing to deploy

---
+++ .next/server/pages/api/hello 2.js
@@ -0,0 +1,31 @@
+(function() {
+var exports = {};
+exports.id = 641;
+exports.ids = [641];
+exports.modules = {
+
+/***/ 2331:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+/* harmony default export */ __webpack_exports__["default"] = ((req, res) => {
+  res.status(200).json({
+    name: 'John Doe'
+  });
+});
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+var __webpack_exports__ = (__webpack_exec__(2331));
+module.exports = __webpack_exports__;
+
+})();
